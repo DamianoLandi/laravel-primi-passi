ba1c253b2b628a77f2b87198f0b6b38d0d64d4f5
DamianoLandi <damianolandi01@gmail.com>
2021-09-30 16:08:27 +0200

    Parametri dinamici

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function (){
-    return 'Hello World!';
+    $name = [
+        'name' => 'Damiano'
+    ];
+    return view('home', $name);
 });
